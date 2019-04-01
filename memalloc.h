e1b4e90308b4c1d7475914433b61ee6e70824827
mnmlyn <mnmlyn@163.com>
2019-04-01 23:11:51 +0800

    添加头文件注释

--- memalloc.h
+++ memalloc.h
@@ -8,8 +8,28 @@
 #ifndef _MEMALLOC_H_
 #define _MEMALLOC_H_
 
+/**
+ * @function    memalloc_init
+ * @brief       内存管理程序初始化
+ * @buf         被管理内存的起始地址
+ * @size        被管理内存的字节数
+ * @szMin       最小可分配内存的字节数，应>=sizeof(int)*2
+ */
 void memalloc_init(unsigned char *buf, int size, int szMin);
+
+/**
+ * @function    memalloc_malloc
+ * @brief       分配一块指定大小的内存，返回获得的内存地址
+ * @size        请求内存的大小
+ * @return      获得的内存的地址
+ */
 void *memalloc_malloc(int size);
+
+/**
+ * @function    memalloc_free
+ * @brief       将分配的内存释放
+ * @ptr         已分配内存的指针
+ */
 void memalloc_free(void *ptr);
 
 #endif
